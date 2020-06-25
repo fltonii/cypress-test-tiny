921a00e09be8215bd35ff7f0673404ac7aede68f
Joao <joao.tonial@sap.com>
2020-06-25 11:24:18 -0300

    adding spec that exposes issue with unavailable id

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,11 @@
-/// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+describe('test', () => {
+  before(() => {
+    cy.server()
   })
+  it("Sample error when XHR id is not available", () => {
+    cy.route("GET", "/api/myService").as("myService");
+    cy.visit("https://cy-test-page.uc.r.appspot.com");
+    cy.wait("@myService");
+    cy.get("#response").should("equal", `{"working":true}`);
+  });
 })
